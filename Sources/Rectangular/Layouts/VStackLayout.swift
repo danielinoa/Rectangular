3f3380b9064c19e6424a19544a62d6836322c966
Daniel Inoa <danielinoa@gmail.com>
2024-01-04 03:16:33 -0800

    Add VStackLayout

---
+++ Sources/Rectangular/Layouts/VStackLayout.swift
@@ -0,0 +1,106 @@
+//
+//  Created by Daniel Inoa on 1/4/24.
+//
+
+public struct VStackLayout: Layout {
+
+    private typealias Priority = Int
+    private typealias SizedItem = (size: Size, item: any LayoutItem)
+    private typealias IndexedItem = (index: Int, item: any LayoutItem)
+
+    public var spacing: Double
+    public var alignment: HorizontalAlignment
+
+    public init(alignment: HorizontalAlignment = .center, spacing: Double = .zero) {
+        self.alignment = alignment
+        self.spacing = spacing
+    }
+
+    public func sizeThatFits(items: [any LayoutItem]) -> Size {
+        let totalInteritemSpacing = totalInteritemSpacing(for: items)
+        let itemsHeight = items.map(\.intrinsicSize.height).reduce(.zero, +)
+        let totalHeight = itemsHeight + totalInteritemSpacing
+        let maxWidth = items.map(\.intrinsicSize.width).max() ?? .zero
+        return .init(width: maxWidth, height: totalHeight)
+    }
+
+    public func sizeThatFits(items: [any LayoutItem], within size: Size) -> Size {
+        let totalInteritemSpacing = totalInteritemSpacing(for: items)
+        let itemsMaxHeight = items.map { $0.sizeThatFits(size).height }.reduce(.zero, +)
+        let fittingHeight = min((itemsMaxHeight + totalInteritemSpacing), size.height)
+        let fittingWidth = items.map { $0.sizeThatFits(size).width }.max() ?? .zero
+        let size = Size(width: fittingWidth, height: fittingHeight)
+        return size
+    }
+    
+    public func frames(for items: [any LayoutItem], within bounds: Rectangle) -> [Rectangle] {
+        var topOffset = bounds.topY
+        let itemSizePairs = sizes(for: items, within: bounds.size)
+        let frames = itemSizePairs.map { pair in
+            let x = Self.leadingOffset(for: pair.size, aligned: alignment, within: bounds)
+            let y = topOffset
+            let frame = Rectangle(x: x, y: y, size: pair.size)
+            topOffset += pair.size.height + spacing
+            return frame
+        }
+        return frames
+    }
+
+    private func sizes(for items: [any LayoutItem], within size: Size) -> [SizedItem] {
+        let pairs: [IndexedItem] = items.enumerated().map { ($0, $1) }
+        var availableHeight = size.height - totalInteritemSpacing(for: items)
+        var sizeTable: [Int: SizedItem] = [:]
+        let priorityGroups: [Priority: [VStackLayout.IndexedItem]] = Dictionary(grouping: pairs, by: \.item.priority)
+        for index in priorityGroups.keys.sorted(by: >) {
+            let group = priorityGroups[index]!
+            let availableSize = Size(width: size.width, height: availableHeight)
+            let (groupSizeTable, remainingHeight) = fittingSizes(for: group, within: availableSize)
+            sizeTable.merge(groupSizeTable) { current, new in current } // No duplicate values are expected.
+            availableHeight = remainingHeight
+        }
+        return sizeTable
+            .sorted { $0.key < $1.key } // ensures that items are in the order they were received.
+            .map { ($0.value.size, $0.value.item) }
+    }
+
+    private func fittingSizes(
+        for pairs: [IndexedItem], within size: Size
+    ) -> (sizeTable: [Priority: SizedItem], remainingHeight: Double) {
+        var sizeTable: [Priority: SizedItem] = .init()
+
+        // The space that remains as views occupy the proposed size.
+        var sharedAvailableHeight = size.height
+
+        // The specified array of views, sorted in ascending manner based on their fitting width.
+        // This ensures that each item gets as much available space as it needs.
+        var heightAscendingPairs = pairs.sorted { $0.item.sizeThatFits(size).height < $1.item.sizeThatFits(size).height }
+
+        // When calculating sizes all views start with an equal amount space within the "shared available space".
+        // Any remaining space unused by a view is then returned to the "shared available space" for other views to use.
+        // In order to ensure no space is wasted in the aforementioned step, the algorithm starts with the thinnest view
+        // and works itself towards the widest view.
+        while !heightAscendingPairs.isEmpty {
+            // An equal amount of space for views yet to be added to the size-table.
+            let equalAllotmentHeight = sharedAvailableHeight / Double(heightAscendingPairs.count)
+            let pair = heightAscendingPairs.removeFirst()
+            let fittingSize = pair.item.sizeThatFits(.init(width: size.width, height: equalAllotmentHeight))
+            sizeTable[pair.index] = (fittingSize, pair.item)
+            sharedAvailableHeight = max(sharedAvailableHeight - fittingSize.height, .zero)
+        }
+        return (sizeTable, sharedAvailableHeight)
+    }
+
+    private static func leadingOffset(for size: Size, aligned: HorizontalAlignment, within bounds: Rectangle) -> Double {
+        let shift: Double
+        switch aligned {
+        case .leading: shift = .zero
+        case .center: shift = (bounds.width - size.width) / 2
+        case .trailing: shift = bounds.width - size.width
+        }
+        return bounds.leadingX + shift
+    }
+
+    private func totalInteritemSpacing(for items: [any LayoutItem]) -> Double {
+        !items.isEmpty ? spacing * Double(items.count - 1) : .zero
+    }
+}
---
+++ Tests/RectangularTests/VStackLayoutTests.swift
@@ -0,0 +1,191 @@
+//
+//  Created by Daniel Inoa on 1/4/24.
+//
+
+import XCTest
+@testable import Rectangular
+
+final class VStackLayoutTests: XCTestCase {
+
+    func test_size_with_no_items() {
+        let layout = VStackLayout()
+        let size = layout.sizeThatFits(items: [])
+        XCTAssertEqual(size, .zero)
+    }
+
+    func test_size_with_no_items_and_with_non_zero_spacing() {
+        var layout = VStackLayout()
+        layout.spacing = 10
+        let size = layout.sizeThatFits(items: [])
+        XCTAssertEqual(size, .zero)
+    }
+
+    func test_size_with_one_item_and_with_non_zero_spacing() {
+        struct FixedItem: LayoutItem {
+            var intrinsicSize: Size { .square(100) }
+            func sizeThatFits(_ size: Size) -> Size { intrinsicSize }
+        }
+        var layout = VStackLayout()
+        layout.spacing = 10
+        let item1 = FixedItem()
+        let size = layout.sizeThatFits(items: [item1])
+        let expected = Size.square(100)
+        XCTAssertEqual(size, expected)
+    }
+
+    func test_size_with_2_flexible_items() {
+        struct Spacer: LayoutItem {
+            func sizeThatFits(_ size: Size) -> Size { size }
+        }
+        let bounds = Size.square(100)
+        let item1 = Spacer()
+        let item2 = Spacer()
+        let layout = VStackLayout()
+        let size = layout.sizeThatFits(items: [item1, item2], within: bounds)
+        let expected = Size.square(100)
+        XCTAssertEqual(size, expected)
+    }
+
+    func test_size_with_2_flexible_items_and_spacing() {
+        struct Spacer: LayoutItem {
+            func sizeThatFits(_ size: Size) -> Size { size }
+        }
+        let bounds = Size.square(100)
+        let item1 = Spacer()
+        let item2 = Spacer()
+        let layout = VStackLayout.init(spacing: 10)
+        let size = layout.sizeThatFits(items: [item1, item2], within: bounds)
+        let expected = Size.square(100)
+        XCTAssertEqual(size, expected)
+    }
+
+    func test_size_with_1_fixed_item() {
+        struct FixedItem: LayoutItem {
+            func sizeThatFits(_ size: Size) -> Size { .init(width: size.width, height: 50) }
+        }
+        let bounds = Size.square(100)
+        let item1 = FixedItem()
+        let layout = VStackLayout()
+        let size = layout.sizeThatFits(items: [item1], within: bounds)
+        let expected = Size.init(width: 100, height: 50)
+        XCTAssertEqual(size, expected)
+    }
+
+    func test_size_with_1_fixed_item_with_spacing() {
+        struct FixedItem: LayoutItem {
+            func sizeThatFits(_ size: Size) -> Size { .init(width: size.width, height: 50) }
+        }
+        let bounds = Size.square(100)
+        let item1 = FixedItem()
+        let layout = VStackLayout(spacing: 10)
+        let size = layout.sizeThatFits(items: [item1], within: bounds)
+        let expected = Size.init(width: 100, height: 50)
+        XCTAssertEqual(size, expected)
+    }
+
+    func test_size_with_2_fixed_items_and_spacing() {
+        struct FixedItem: LayoutItem {
+            func sizeThatFits(_ size: Size) -> Size { .init(width: size.width, height: 20) }
+        }
+        let bounds = Size.square(100)
+        let item1 = FixedItem()
+        let item2 = FixedItem()
+        let layout = VStackLayout(spacing: 10)
+        let size = layout.sizeThatFits(items: [item1, item2], within: bounds)
+        let expected = Size.init(width: 100, height: 50)
+        XCTAssertEqual(size, expected)
+    }
+
+    func test_frames_of_2_fixed_items_and_spacing() throws {
+        struct FixedItem: LayoutItem {
+            func sizeThatFits(_ size: Size) -> Size { .square(20) }
+        }
+        let item1 = FixedItem()
+        let item2 = FixedItem()
+        let bounds = Rectangle(origin: .zero, size: .square(100))
+        let layout = VStackLayout(spacing: 10)
+        let frames = layout.frames(for: [item1, item2], within: bounds)
+
+        XCTAssertEqual(frames.first!.x, 40)
+        XCTAssertEqual(frames.first!.y, 0)
+        XCTAssertEqual(frames.last!.x, 40)
+        XCTAssertEqual(frames.last!.y, 30)
+    }
+
+    func test_frames_with_fixed_and_flexible_item() {
+        struct FixedItem: LayoutItem {
+            func sizeThatFits(_ size: Size) -> Size { .init(width: size.width, height: 25) }
+        }
+        struct FlexibleItem: LayoutItem {
+            func sizeThatFits(_ size: Size) -> Size { size }
+        }
+        let bounds = Rectangle(origin: .zero, size: .square(100))
+        let layout = VStackLayout()
+        let items: [any LayoutItem] = [FixedItem(), FlexibleItem()]
+        let frames = layout.frames(for: items, within: bounds)
+
+        XCTAssertEqual(frames.first!.y, 0)
+        XCTAssertEqual(frames.first!.height, 25)
+        XCTAssertEqual(frames.last!.y, 25)
+        XCTAssertEqual(frames.last!.height, 75)
+    }
+
+    func test_frames_with_flexible_and_fixed_item() {
+        struct FixedItem: LayoutItem {
+            func sizeThatFits(_ size: Size) -> Size { .init(width: size.width, height: 25) }
+        }
+        struct FlexibleItem: LayoutItem {
+            func sizeThatFits(_ size: Size) -> Size { size }
+        }
+        let bounds = Rectangle(origin: .zero, size: .square(100))
+        let layout = VStackLayout()
+        let items: [any LayoutItem] = [FlexibleItem(), FixedItem()]
+        let frames = layout.frames(for: items, within: bounds)
+
+        XCTAssertEqual(frames.first!.y, 0)
+        XCTAssertEqual(frames.first!.height, 75)
+        XCTAssertEqual(frames.last!.y, 75)
+        XCTAssertEqual(frames.last!.height, 25)
+    }
+
+    func test_frames_with_fixed_item_between_flexible_items() {
+        struct FixedItem: LayoutItem {
+            func sizeThatFits(_ size: Size) -> Size { .init(width: size.width, height: 30) }
+        }
+        struct FlexibleItem: LayoutItem {
+            func sizeThatFits(_ size: Size) -> Size { size }
+        }
+        let bounds = Rectangle(origin: .zero, size: .square(100))
+        let layout = VStackLayout()
+        let items: [any LayoutItem] = [FlexibleItem(), FixedItem(), FlexibleItem()]
+        let frames = layout.frames(for: items, within: bounds)
+
+        XCTAssertEqual(frames[0].y, 0)
+        XCTAssertEqual(frames[0].height, 35)
+        XCTAssertEqual(frames[1].y, 35)
+        XCTAssertEqual(frames[1].height, 30)
+        XCTAssertEqual(frames[2].y, 65)
+        XCTAssertEqual(frames[2].height, 35)
+    }
+
+    func test_frames_where_flexible_item_has_higher_priority_than_fixed_item() {
+        struct FixedItem: LayoutItem {
+            func sizeThatFits(_ size: Size) -> Size { .init(width: 30, height: 30) }
+        }
+        struct FlexibleItem: LayoutItem {
+            var priority: Int { 1 }
+            func sizeThatFits(_ size: Size) -> Size { size }
+        }
+        let bounds = Rectangle(origin: .zero, size: .square(100))
+        let layout = VStackLayout()
+        let items: [any LayoutItem] = [FlexibleItem(), FixedItem()]
+        let frames = layout.frames(for: items, within: bounds)
+
+        XCTAssertEqual(frames[0].y, 0)
+        XCTAssertEqual(frames[0].width, 100)
+        XCTAssertEqual(frames[0].height, 100)
+        XCTAssertEqual(frames[1].y, 100)
+        XCTAssertEqual(frames[1].width, 30)
+        XCTAssertEqual(frames[1].height, 30)
+    }
+}
